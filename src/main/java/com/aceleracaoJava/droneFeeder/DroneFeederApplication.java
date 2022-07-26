909ca4b8bf1fc645eef65f6f3f7cbb4ecafb2571
pedrobarreto <pbdeoliveira@gmail.com>
2022-07-26 18:45:25 -0300

    chore: installing maven

---
+++ src/main/java/com/aceleracaoJava/droneFeeder/DroneFeederApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracaoJava.droneFeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneFeederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleracaoJava/droneFeeder/DroneFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleracaoJava.droneFeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
