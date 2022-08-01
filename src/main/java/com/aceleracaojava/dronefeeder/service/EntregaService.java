9cdc42af9978ec66c17badeeda1bcabfd44ee05e
pedrobarreto <pbdeoliveira@gmail.com>
2022-08-01 00:05:39 -0300

    feat: entrega service exceptions

--- src/main/java/com/aceleracaojava/dronefeeder/service/EntregaService.java
+++ src/main/java/com/aceleracaojava/dronefeeder/service/EntregaService.java
@@ -3,11 +3,14 @@ package com.aceleracaojava.dronefeeder.service;
 import com.aceleracaojava.dronefeeder.dto.EntregaDto;
 import com.aceleracaojava.dronefeeder.entity.Drone;
 import com.aceleracaojava.dronefeeder.entity.Entrega;
+import com.aceleracaojava.dronefeeder.exceptions.DroneNaoEncontradoException;
+import com.aceleracaojava.dronefeeder.exceptions.EntregaNaoEncontradaException;
 import com.aceleracaojava.dronefeeder.repository.DroneRepository;
 import com.aceleracaojava.dronefeeder.repository.EntregaRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 /**
  * EntregaService.
@@ -23,39 +26,54 @@ public class EntregaService implements ServiceInterface<EntregaDto, Entrega> {
 
   @Override
   public Entrega create(EntregaDto object) {
-    // TODO Auto-generated method stub
-    Drone drone = droneRepo.findById(object.getDroneId()).get();
-    Entrega newEntrega = new Entrega();
-    drone.addEntrega(newEntrega);
-    Drone saved = droneRepo.save(drone);
+    Assert.notNull(object.getDroneId(), "Drone Id não pode estar em branco");
+    try {
+      Drone drone = droneRepo.findById(object.getDroneId()).get();
+      Entrega newEntrega = new Entrega();
+      drone.addEntrega(newEntrega);
+      Drone saved = droneRepo.save(drone);
     return saved.getEntregas().get(saved.getEntregas().size() - 1);
+    } catch (Exception e) {
+      throw new DroneNaoEncontradoException(object.getDroneId().toString());
+    }
   }
 
   @Override
   public Entrega findById(Long id) {
-    // TODO Auto-generated method stub
-    return repository.findById(id).get();
+    try {
+      return repository.findById(id).get();
+    } catch (Exception e) {
+      throw new EntregaNaoEncontradaException(id.toString());
+    }
   }
 
   @Override
   public List<Entrega> findAll() {
-    // TODO Auto-generated method stub
+    if(repository.findAll().isEmpty()) {
+      throw new EntregaNaoEncontradaException("Tente novamente.");
+    }
     return repository.findAll();
   }
 
   @Override
   public void update(Long id, EntregaDto object) {
-    // TODO Auto-generated method stub
-    Entrega toUpdate = repository.findById(id).get();
-    toUpdate.setData(object.getData());
-    toUpdate.setStatus(object.getStatus());
-    repository.save(toUpdate);
+    try {
+      Entrega toUpdate = repository.findById(id).get();
+      toUpdate.setStatus(object.getStatus());
+      repository.save(toUpdate);
+    } catch (Exception e) {
+      throw new EntregaNaoEncontradaException(id.toString());
+    }
   }
 
   @Override
   public void delete(Long id) {
-    // TODO Auto-generated method stub
-    repository.deleteById(id);
+    try {
+      repository.deleteById(id);
+    } catch (Exception e) {
+      throw new EntregaNaoEncontradaException(id.toString());
+    }
+
 
   }
 
