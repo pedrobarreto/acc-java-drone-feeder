0eff0dd8762fa791b06ae345ebe7762b7697eefb
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-08-03 16:26:41 -0300

    fix: Ajusta ControllerInterface para retornar status http corretos

--- backend/src/main/java/com/aceleracaojava/dronefeeder/controller/ControllerInterface.java
+++ backend/src/main/java/com/aceleracaojava/dronefeeder/controller/ControllerInterface.java
@@ -2,18 +2,20 @@ package com.aceleracaojava.dronefeeder.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 /**
  * ControllerInterface.
  */
 public interface ControllerInterface<T, K> {
-  public K create(T object);
+  public ResponseEntity<K> create(T object);
 
   public K findById(Long id);
 
   public List<K> findAll();
 
-  public void update(Long id, T object);
+  public ResponseEntity<Object> update(Long id, T object);
 
-  public void delete(Long id);
+  public ResponseEntity<Object> delete(Long id);
 
 }
